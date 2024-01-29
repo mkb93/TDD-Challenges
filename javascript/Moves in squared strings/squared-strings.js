560a0fa37859d2451c9c9bcea7a0056077a614f9
marco belahouane <nfk18@hotmail.co.uk>
2024-01-29 13:30:40 +0000

    pseudo code for movees in squared strings

--- javascript/Moves in squared strings/squared-strings.js
+++ javascript/Moves in squared strings/squared-strings.js
@@ -1,11 +1,17 @@
 function vertMirror(strng) {
-  // 
+  //  the plan is to seperate the larger string by the /n 
+  //  reverse the order of the letters in each element
+  //  then join it and returning it 
 }
 function horMirror(strng) {
-  // 
+  // the plan is to seperate the larger string into smaller strings by /n
+  // reverse the order of the elements in the Array
+  // then rejoin it and return it  
 }
-function oper(fct, s) {
-  // Your code
+function oper(func, s) {
+  // the plan is to use the first argument func
+  // pass the second argument s into func
+  // then return the value
 }
 
 module.exports = {vertMirror, horMirror, oper}
