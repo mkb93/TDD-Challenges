f3e89033c2a4c00aefaeff48d8b3ff766042ba58
marco belahouane <nfk18@hotmail.co.uk>
2024-02-01 20:58:23 +0000

    first failure in squared strings it doesnt reverse a string

--- javascript/Moves in squared strings/squared-strings.js
+++ javascript/Moves in squared strings/squared-strings.js
@@ -1,17 +1,19 @@
-function vertMirror(strng) {
-  //  the plan is to seperate the larger string by the /n 
-  //  reverse the order of the letters in each element
-  //  then join it and returning it 
+function vertMirror(str) {
+  console.log(str);
+  return "dcba\nhgfe\nlkji\nponm";
 }
 function horMirror(strng) {
   // the plan is to seperate the larger string into smaller strings by /n
   // reverse the order of the elements in the Array
-  // then rejoin it and return it  
+  // then rejoin it and return it
 }
 function oper(func, s) {
   // the plan is to use the first argument func
   // pass the second argument s into func
   // then return the value
+  return func(s);
 }
 
-module.exports = {vertMirror, horMirror, oper}
+vertMirror("abcd\nefgh\nijkl\nmnop");
+
+module.exports = { vertMirror, horMirror, oper };
--- javascript/Moves in squared strings/squared-strings.test.js
+++ javascript/Moves in squared strings/squared-strings.test.js
@@ -1,3 +1,5 @@
-const {vertMirror, horMirror, oper} =require('./squared-strings.js')
+const { vertMirror, horMirror, oper } = require("./squared-strings.js");
 
-console.log(oper(vertMirror,'abcd\nefgh\nijkl\nmnop')=== 'dcba\nhgfe\nlkji\nponm')
+test("'asda' returns 'adsa'", () => {
+  expect(oper(vertMirror, "asda")).toBe("adsa");
+});
