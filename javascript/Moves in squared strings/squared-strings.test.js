72cd983d3a29178cafa676ce2a9cb7e1824bc3c6
marco belahouane <nfk18@hotmail.co.uk>
2024-02-03 18:41:15 +0200

    test added to vertMirror multiple \n in a row passes

--- javascript/Moves in squared strings/squared-strings.test.js
+++ javascript/Moves in squared strings/squared-strings.test.js
@@ -10,6 +10,9 @@ describe("are the strings reversed with the vertMirror function", () => {
   test("expect all the individual strings seperated by \n to be reversed", () => {
     expect(oper(vertMirror, "forget\ntable")).toBe("tegrof\nelbat");
   });
+  test("expect all the individual strings seperated by \n to be reversed", () => {
+    expect(oper(vertMirror, "forget\n\ntable")).toBe("tegrof\n\nelbat");
+  });
 });
 describe("is the horMirror function reveresing the position of the strings in the larger string", () => {
   test("'asda' returns 'asda'", () => {
