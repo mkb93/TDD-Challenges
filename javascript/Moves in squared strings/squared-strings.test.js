1f096222dc479b4252b2357c413fd860db300949
marco belahouane <nfk18@hotmail.co.uk>
2024-02-03 18:34:18 +0200

    horMirror function doesnt for a single string in a larger

--- javascript/Moves in squared strings/squared-strings.test.js
+++ javascript/Moves in squared strings/squared-strings.test.js
@@ -11,3 +11,14 @@ describe("are the strings reversed with the vertMirror function", () => {
     expect(oper(vertMirror, "forget\ntable")).toBe("tegrof\nelbat");
   });
 });
+describe("is the horMirror function reveresing the position of the strings in the larger string", () => {
+  test("'asda' returns 'asda'", () => {
+    expect(oper(horMirror, "asda")).toBe("asda");
+  });
+  // test("expect 'forget' to be reversed", () => {
+  //   expect(oper(vertMirror, "forget")).toBe("tegrof");
+  // });
+  // test("expect all the individual strings seperated by \n to be reversed", () => {
+  //   expect(oper(vertMirror, "forget\ntable")).toBe("tegrof\nelbat");
+  // });
+});
