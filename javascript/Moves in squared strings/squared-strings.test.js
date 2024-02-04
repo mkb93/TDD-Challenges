b73119c31af56ea07615e8e6c784a2e95a117b77
marco belahouane <nfk18@hotmail.co.uk>
2024-02-04 06:06:22 +0200

    added and passed empty string test to vertMirror tests

--- javascript/Moves in squared strings/squared-strings.test.js
+++ javascript/Moves in squared strings/squared-strings.test.js
@@ -13,6 +13,9 @@ describe("are the strings reversed with the vertMirror function", () => {
   test("expect all the individual strings seperated by \n to be reversed", () => {
     expect(oper(vertMirror, "forget\n\ntable")).toBe("tegrof\n\nelbat");
   });
+  test("expect an empty string to return an empty string", () => {
+    expect(oper(vertMirror, "")).toBe("");
+  });
 });
 describe("is the horMirror function reveresing the position of the strings in the larger string", () => {
   test("'asda' returns 'asda'", () => {
@@ -22,4 +25,8 @@ describe("is the horMirror function reveresing the position of the strings in th
   test("expect two strings to switch order", () => {
     expect(oper(horMirror, "forget\ntable")).toBe("table\nforget");
   });
+
+  // test("expect more than two  strings to switch order", () => {
+  //   expect(oper(horMirror, "forget\ntable")).toBe("table\nforget");
+  // });
 });
